c0e065d019f4704d9ae819f24d095438af3ef339
Jack Connolly <66589746+Hunter0907@users.noreply.github.com>
2022-10-15 12:17:32 +0100

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -7,27 +7,27 @@ import random
 import numpy as np
 
 
-x = range(0,250)
+x = range(0,400)
 
-matrix_size= x
+matrix_size = x
 time_taken=[]
 
 
-for num in matrix_size:
+for number in matrix_size:
     start = perf_counter()
-    a=[]
-    b=[]
-    for j in range(num):
+    w=[]
+    z=[]
+    for s in range(number):
         a_row=[]
         b_row=[]
-        for k in range(num):
+        for i in range(number):
             a_row.append(random.randint(0,9))
             b_row.append(random.randint(0,9))
-        a.append(a_row)
-        b.append(b_row)
+        w.append(a_row)
+        z.append(b_row)
 
         
-    result=[[sum(x*y for x,y in zip(row_a,col_b)) for col_b in zip(*b)]for row_a in a]
+    result=[[sum(x*y for x,y in zip(row_a,col_b)) for col_b in zip(*z)]for row_a in w]
     end = perf_counter()
     time_taken_run = end - start
     time_taken.append(time_taken_run)
@@ -36,10 +36,10 @@ print(time_taken)
 
 print("Total Time is",sum(time_taken),"seconds")
 
-xpoints = np.array(matrix_size)
-ypoints = np.array(time_taken)
+x_points = np.array(matrix_size)
+y_points = np.array(time_taken)
 
-plt.plot(xpoints, ypoints)
+plt.plot(x_points, y_points)
 plt.show()
 
 
